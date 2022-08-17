6ad6d96145ebc14101ea4f2869bcb0c06c367ca5
Pete Steward <p.steward@cgiar.org>
2022-08-17 07:16:20 +0000

    AC process market access

---
+++ R/process_market_access.R
@@ -0,0 +1,68 @@
+require(terra)
+
+# Root of file structure
+DataDir<-"/home/jovyan/common_data"
+
+# Create intermediate directory
+DataDirInt<-paste0(DataDir,"/atlas_market_access/intermediate")
+if(!dir.exists(DataDirInt)){
+    dir.create(DataDirInt)
+    }
+    
+# Read in admin1 for subsaharan africa
+adm1_africa<-terra::vect(paste0(DataDir,"/atlas_boundaries/intermediate/gadm41_ssa_1.shp"))
+
+# Read in a base raster
+base_raster<-terra::rast(paste0(DataDir,"/mapspam_2017/raw/spam2017V2r1_SSA_H_YAMS_S.tif"))
+base_raster<-terra::crop(base_raster,adm1_africa)
+
+# Read in market access data
+market_access<-terra::rast(paste0(DataDir,"/atlas_market_access/raw/2015_accessibility_to_cities_v1.0.tif"))
+
+# Resample
+market_access<-terra::resample(market_access,base_raster,method="average")
+
+# Crop and mask
+data<-terra::mask(terra::crop(market_access,adm1_africa),adm1_africa)
+names(data)<-"market_access"
+
+# Save data
+terra::writeRaster(data,file=paste0(DataDirInt,"/",names(data),".tif"),overwrite=T)
+
+# Generate terciles
+Overwrite<-T
+# Adaptive capacity is classified thus: high = good, low = bad
+# Low distance to market is better, lower tercile = good, upper tercile = bad 
+Invert<-T
+
+Labels<-c("low","medium","high")
+Values<-c(0,1,2)
+
+data_terciles<-terra::rast(lapply(names(data),FUN=function(FIELD){
+    
+    File<-paste0(DataDirInt,"/",FIELD,"_terciles.tif")
+    
+    if((!file.exists(File))|Overwrite){
+        X<-data[[FIELD]]
+        vTert = quantile(X[], c(0:3/3),na.rm=T)
+
+       Levels<-if(Invert){Labels[length(Labels):1]}else{Labels}
+                
+        Intervals<-data.frame(Intervals=apply(cbind(Levels,round(vTert[1:3],3),round(vTert[2:4],3)),1,paste,collapse="-"))
+        write.csv(Intervals,file=paste0(DataDirInt,"/",FIELD,"_terciles.csv"),row.names=F)
+        
+        Terciles<-cut(X[],
+                      vTert, 
+                      include.lowest = T, 
+                      labels = Values)
+        
+        X[]<-as.numeric(Terciles)-1
+        levels(X)<-  Levels  
+        suppressWarnings(terra::writeRaster(X,file=File,overwrite=T))
+        X
+        }else{
+            terra::rast(File)
+    }
+}))
+
+terra::plot(data_terciles)
