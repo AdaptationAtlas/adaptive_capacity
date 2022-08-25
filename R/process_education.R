9b5fffcd95d3f97bd638a5f81d3e43933c3fef7d
Pete Steward <p.steward@cgiar.org>
2022-08-25 13:39:41 +0000

    Cumulative quantile layers added

--- R/process_education.R
+++ R/process_education.R
@@ -29,7 +29,7 @@ data<-terra::rast(paste0(DataDir,"/atlas_education/raw/shdi-2018-education.tif")
 
 # 0 is an NA value is this dataset
 data[data==0]<-NA
-names(data)<-"education"
+names(data)<-"Education"
 
 # Resample data
 data<-terra::resample(data,base_raster,method="average")
@@ -46,29 +46,19 @@ lapply(names(data),FUN=function(FIELD){
 Overwrite=T
 # Adaptive capacity is classified thus: high = good, low = bad
 # High education is better, lower tercile = bad, upper tercile = good 
-Levels<-c("low","medium","high")
+Invert=F
 
-data_terciles<-terra::rast(lapply(names(data),FUN=function(FIELD){
-    
-    File<-paste0(DataDirInt,"/",FIELD,"_terciles.tif")
-    
-    if((!file.exists(File))|Overwrite){
-        X<-data[[FIELD]]
-        vTert = quantile(X[], c(0:3/3),na.rm=T)
-        Intervals<-data.frame(Intervals=apply(cbind(Levels,round(vTert[1:3],3),round(vTert[2:4],3)),1,paste,collapse="-"))
-        write.csv(Intervals,file=paste0(DataDirInt,"/",FIELD,"_terciles.csv"),row.names=F)
-        
-        Terciles<-cut(X[],
-                      vTert, 
-                      include.lowest = T, 
-                      labels = c(0,1,2))
-        X[]<-as.numeric(Terciles)-1
-        levels(X)<-Levels   
-        suppressWarnings(terra::writeRaster(X,file=File,overwrite=T))
-        X
-        }else{
-            terra::rast(File)
-    }
-}))
+devtools::source_url("https://github.com/AdaptationAtlas/adaptive_capacity/blob/main/R/functions.R?raw=TRUE")
+
+data_terciles<-quantile_split(data,
+                              Labels=c("low","medium","high"),
+                              Invert=Invert,
+                              Overwrite=T,
+                              do_binary_splits=T,
+                              savedir=DataDirInt)
 
 terra::plot(c(data,data_terciles))
+
+Files<-list.files(DataDirInt,".tif",full.names=T)
+Files<-Files[!grepl("aux",Files)]
+terra::plot(terra::rast(Files))
