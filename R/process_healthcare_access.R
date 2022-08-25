35b854a76d7eb454e5a73e874a44942669601960
Pete Steward <p.steward@cgiar.org>
2022-08-25 13:39:53 +0000

    Cumulative quantile layers added

--- R/process_healthcare_access.R
+++ R/process_healthcare_access.R
@@ -28,8 +28,8 @@ water_mask<-terra::mask(terra::crop(water_mask,adm1_africa),adm1_africa)
 hc_access_walking<-terra::rast(paste0(DataDir,"/atlas_healthcare_access/raw/2020_walking_only_travel_time_to_healthcare.geotiff"))
 hc_access_motor<-terra::rast(paste0(DataDir,"/atlas_healthcare_access/raw/2020_motorized_travel_time_to_healthcare.geotiff"))
 
-data<-c(hc_access_walking,hc_access_motor)
-names(data)<-c("healthcare_access_walking","healthcare_access_motorized")
+data<-hc_access_motor
+names(data)<-"Health_Facility_Access"
 
 # Resample
 data<-terra::resample(data,base_raster,method="average")
@@ -47,34 +47,17 @@ Overwrite=T
 # Low distance to healthcare is better, lower tercile = good, upper tercile = bad 
 Invert<-T
 
-Labels<-c("low","medium","high")
-Values<-c(0,1,2)
+devtools::source_url("https://github.com/AdaptationAtlas/adaptive_capacity/blob/main/R/functions.R?raw=TRUE")
 
-data_terciles<-terra::rast(lapply(names(data),FUN=function(FIELD){
-    
-    File<-paste0(DataDirInt,"/",FIELD,"_terciles.tif")
-    
-    if((!file.exists(File))|Overwrite){
-        X<-data[[FIELD]]
-        vTert = quantile(X[], c(0:3/3),na.rm=T)
-
-       Levels<-if(Invert){Labels[length(Labels):1]}else{Labels}
-                
-        Intervals<-data.frame(Intervals=apply(cbind(Levels,round(vTert[1:3],3),round(vTert[2:4],3)),1,paste,collapse="-"))
-        write.csv(Intervals,file=paste0(DataDirInt,"/",FIELD,"_terciles.csv"),row.names=F)
-        
-        Terciles<-cut(X[],
-                      vTert, 
-                      include.lowest = T, 
-                      labels = Values)
-        
-        X[]<-as.numeric(Terciles)-1
-        levels(X)<-  Levels  
-        suppressWarnings(terra::writeRaster(X,file=File,overwrite=T))
-        X
-        }else{
-            terra::rast(File)
-    }
-}))
+data_terciles<-quantile_split(data,
+                              Labels=c("low","medium","high"),
+                              Invert=Invert,
+                              Overwrite=T,
+                              do_binary_splits=T,
+                              savedir=DataDirInt)
 
 terra::plot(c(data,data_terciles))
+
+Files<-list.files(DataDirInt,".tif",full.names=T)
+Files<-Files[!grepl("aux",Files)]
+terra::plot(terra::rast(Files))
