c68bc3c3dc490b6aa05fbb793343219bf91445d7
Pete Steward <p.steward@cgiar.org>
2022-08-25 13:39:59 +0000

    Cumulative quantile layers added

--- R/process_market_access.R
+++ R/process_market_access.R
@@ -38,39 +38,19 @@ names(data)<-"market_access"
 terra::writeRaster(data,file=paste0(DataDirInt,"/",names(data),".tif"),overwrite=T)
 
 # Generate terciles
-Overwrite<-T
 # Adaptive capacity is classified thus: high = good, low = bad
 # Low distance to market is better, lower tercile = good, upper tercile = bad 
 Invert<-T
 
-Labels<-c("low","medium","high")
-Values<-c(0,1,2)
+devtools::source_url("https://github.com/AdaptationAtlas/adaptive_capacity/blob/main/R/functions.R?raw=TRUE")
 
-data_terciles<-terra::rast(lapply(names(data),FUN=function(FIELD){
-    
-    File<-paste0(DataDirInt,"/",FIELD,"_terciles.tif")
-    
-    if((!file.exists(File))|Overwrite){
-        X<-data[[FIELD]]
-        vTert = quantile(X[], c(0:3/3),na.rm=T)
+data_terciles<-quantile_split(data,
+                              Labels=c("low","medium","high"),
+                              Invert=Invert,
+                              Overwrite=T,
+                              do_binary_splits=T,
+                              savedir=DataDirInt)
 
-       Levels<-if(Invert){Labels[length(Labels):1]}else{Labels}
-                
-        Intervals<-data.frame(Intervals=apply(cbind(Levels,round(vTert[1:3],3),round(vTert[2:4],3)),1,paste,collapse="-"))
-        write.csv(Intervals,file=paste0(DataDirInt,"/",FIELD,"_terciles.csv"),row.names=F)
-        
-        Terciles<-cut(X[],
-                      vTert, 
-                      include.lowest = T, 
-                      labels = Values)
-        
-        X[]<-as.numeric(Terciles)-1
-        levels(X)<-  Levels  
-        suppressWarnings(terra::writeRaster(X,file=File,overwrite=T))
-        X
-        }else{
-            terra::rast(File)
-    }
-}))
-
-terra::plot(c(data,data_terciles))
+Files<-list.files(DataDirInt,".tif",full.names=T)
+Files<-Files[!grepl("aux",Files)]
+terra::plot(terra::rast(Files))
